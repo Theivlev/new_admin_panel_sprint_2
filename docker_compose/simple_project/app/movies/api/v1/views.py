1ed488f1b3200ffd8707d1cb72a15eb54801aa3a
Ivlev Alexey <theivlev@yandex.ru>
2024-12-18 13:12:32 +0400

    Добавил MoviesMixin

--- docker_compose/simple_project/app/movies/api/v1/views.py
+++ docker_compose/simple_project/app/movies/api/v1/views.py
@@ -8,11 +8,56 @@ from movies.models import FilmWork
 
 
 class MoviesApiMixin:
+    
     model = FilmWork
     http_method_names = ['get']
 
+    @property
+    def queryset(self):
+        """
+        Базовый набор данных о фильмах.
+        Возвращает только необходимые поля из FilmWork с предзагрузкой связанных данных.
+        """
+        return FilmWork.objects.prefetch_related('genres', 'persons').values(
+            'id',
+            'title',
+            'description',
+            'rating',
+            'type',
+            'creation_date'
+        )
+
     def get_queryset(self):
-        return  # Сформированный QuerySet
+        """
+        Возвращает аннотированный набор данных о фильмах.
+        Агрегирует жанры и участников по ролям (актеры, режиссеры, сценаристы).
+        """
+        genres = ArrayAgg(
+            'genres__name',
+            distinct=True
+        )
+        actors = ArrayAgg(
+            'persons__full_name',
+            distinct=True,
+            filter=Q(personfilmwork__role='actor')
+        )
+        directors = ArrayAgg(
+            'persons__full_name',
+            distinct=True,
+            filter=Q(personfilmwork__role='director')
+        )
+        writers = ArrayAgg(
+            'persons__full_name',
+            distinct=True,
+            filter=Q(personfilmwork__role='writer')
+        )
+
+        return self.queryset.annotate(
+            genres=genres,
+            actors=actors,
+            directors=directors,
+            writers=writers
+        )
 
     def render_to_response(self, context, **response_kwargs):
         return JsonResponse(context)
