d60959a1a83a965c132ee820adc75faa8ddaf899
Ivlev Alexey <theivlev@yandex.ru>
2024-12-18 10:50:50 +0400

    Изменил конфигурацию_

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -16,8 +16,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
-DEBUG = os.getenv('DEBUG', False) == 'True'
-
+#DEBUG = os.getenv('DEBUG', False) == 'True'
+DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
